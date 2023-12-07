7e7f01e1943a857b6ee4a21759303c9ae80bb0ef
Peter Lee <peterlee.works@gmail.com>
2023-12-08 03:52:20 +0800

    delete console info

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { execSql } from "@/_database/db";
 
 export async function POST(request: NextRequest) {
-  console.log(request.nextUrl.searchParams);
+  // console.log(request.nextUrl.searchParams);
   const res = await execSql("select * from user");
   return NextResponse.json(res);
 }
