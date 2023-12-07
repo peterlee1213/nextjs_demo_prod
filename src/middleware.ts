745ad16589d2cf34daed43a8debd7515b90a3332
Peter Lee <peterlee.works@gmail.com>
2023-12-08 03:49:46 +0800

    delete console info

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const middleware = (request: NextRequest) => {
-  console.log(request.nextUrl.pathname, "1");
+  // console.log(request.nextUrl.pathname, "1");
   return NextResponse.next();
 };
 
