51456d9730390efe6f079da9eebb68431532452d
Peter Lee <peterlee.works@gmail.com>
2023-12-08 03:51:47 +0800

    add non-production environment snackbar

--- src/app/(login&signup)/layout.tsx
+++ src/app/(login&signup)/layout.tsx
@@ -3,20 +3,24 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./global.css";
-import { Card, CardHeader, CardContent } from "@mui/material";
+import { Card, CardHeader, CardContent, Snackbar, Alert } from "@mui/material";
+import { NODE_ENV } from "@/envParams";
 
 export default function RootLayout({
-  children,
   title,
   content,
 }: {
-  children: React.ReactNode;
   title: React.ReactNode;
   content: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
+        <Snackbar open={NODE_ENV !== "production"}>
+          <Alert variant="filled" severity="info" sx={{ width: "100%" }}>
+            {`current NODE_ENV=${NODE_ENV}, Any operations here won't affect production environment`}
+          </Alert>
+        </Snackbar>
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96">
           <Card>
             <CardHeader
